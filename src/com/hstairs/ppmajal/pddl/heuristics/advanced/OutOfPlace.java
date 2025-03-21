24b3a0014296fc8eeedbaad65aae596214e98483
zkac280 <zkac280@live.rhul.ac.uk>
2025-03-21 17:07:12 +0000

    modifying code to take the total_doctor_cost as a weight to determine the max cost

--- src/com/hstairs/ppmajal/pddl/heuristics/advanced/OutOfPlace.java
+++ src/com/hstairs/ppmajal/pddl/heuristics/advanced/OutOfPlace.java
@@ -1,24 +1,21 @@
 package com.hstairs.ppmajal.pddl.heuristics.advanced;
 
-import java.util.ArrayList;
+import java.util.Collection;
 
+import com.hstairs.ppmajal.PDDLProblem.PDDLProblem;
+import com.hstairs.ppmajal.PDDLProblem.PDDLState;
 import com.hstairs.ppmajal.conditions.AndCond;
 import com.hstairs.ppmajal.conditions.Condition;
-import com.hstairs.ppmajal.PDDLProblem.PDDLProblem;
+import com.hstairs.ppmajal.expressions.NumEffect;
+import com.hstairs.ppmajal.expressions.NumFluent;
 import com.hstairs.ppmajal.problem.State;
 import com.hstairs.ppmajal.search.SearchHeuristic;
+import com.hstairs.ppmajal.transition.Transition;
 import com.hstairs.ppmajal.transition.TransitionGround;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
-import com.hstairs.ppmajal.expressions.NumFluent;
-import com.hstairs.ppmajal.expressions.PDDLNumber;
 
 /**
  *
- * @author enrico
+ * @author enrico, sophie Kua
  */
 public class OutOfPlace implements SearchHeuristic {
 
@@ -31,15 +28,17 @@ public class OutOfPlace implements SearchHeuristic {
     // calculates an estimate for how far away the current state is from the goal
     @Override
     public float computeEstimate(State s) {
-        int c = 0;
-        float numDiff = 0;
+        int missingGoals = 0;
+        double futureDoctorCost = 0.0;
+        double currentDoctorCost = getStateTotalDoctorCost((PDDLState) s);
+
         Condition goals = this.problem.getGoals();
 
         if (goals instanceof AndCond) {
             for (Object c1 : ((AndCond) goals).sons) {
                 Condition con = (Condition) c1;
                 if (!s.satisfy(con)) { // if the state doesn't equal the goal condition then add 1 to c
-                    c++;
+                    missingGoals++;
 
                 }
                 
@@ -47,39 +46,56 @@ public class OutOfPlace implements SearchHeuristic {
 
         }
 
-        List<Double> stateNumFluent = s.getNumFluents();
-        List NumFluentTest = s.getNumFluents();
-        NumFluent NumFluentRef = this.problem.getNumfluentReference("total_doctor_cost");
-        System.out.println(NumFluentTest);
-        System.out.println(NumFluentRef);
+        for (Transition transition : problem.getTransitions()) {
+            // Get all numeric effects from this transition
+            Collection<NumEffect> numericEffects = transition.getAllNumericEffects();
 
-        Set<NumFluent> goalFluents = problem.getGoals().getInvolvedFluents();
-        //System.out.println(goalFluents);
+            for (NumEffect numEffect : numericEffects) {
+                // Check if the numeric effect involves the "total_doctor_cost" fluent
+                if (numEffect.getFluentAffected().getName().equals("total_doctor_cost")) {
+                    // Evaluate the numeric effect on the fluent
+                    double effectValue = numEffect.getRight().eval(s);
+                    System.out.println(effectValue);
 
-        List<Double> goalNumFluent = new ArrayList<>();
-        
-
-        for (NumFluent nf : goalFluents) {
-            PDDLNumber goalPDDLNumber = problem.getInitNumFluentsValues().get(nf);
-            double goalValue = (goalPDDLNumber != null) ? goalPDDLNumber.getNumber().doubleValue() : 0.0;
-            goalNumFluent.add(goalValue);
-        }
-        
-        //System.out.println(goalFluents);
-
-        int minSize = Math.min(stateNumFluent.size(), goalNumFluent.size());
-        //System.out.println(minSize);
-
-        for (int i = 0; i < minSize; i++) {
-            numDiff += Math.abs(stateNumFluent.get(i) - goalNumFluent.get(i));
-            System.out.printf("state fluent: %.2f\n", stateNumFluent.get(i));
+                    futureDoctorCost += effectValue;
+                }
+            }
         }
 
-        float hValue = c + numDiff;
-        System.out.printf("numDiff: %f\n", numDiff);
+        System.out.println(futureDoctorCost);
+        System.out.println(currentDoctorCost);
+
+        float cost = (float) currentDoctorCost + (float) futureDoctorCost;
+        float hValue = missingGoals + (cost / 10);
+
+        System.out.printf("doctorCost: %f\n", cost);
         System.out.printf("Out of Place Value: %f\n", hValue);
         return hValue;
     }
+
+    public double getStateTotalDoctorCost(State s) {
+        PDDLState pddlState = (PDDLState) s;  
+        // Look for the numeric fluent "total_doctor_cost"
+        NumFluent doctorCostFluent = (NumFluent) this.problem.getNumFluents().stream().filter(f -> ((NumFluent) f).getName().equals("total_doctor_cost")).findFirst().orElse(null);
+        System.out.println(doctorCostFluent);
+
+        if (doctorCostFluent != null) {
+            // Find the correct numeric fluent ID in the state
+            int[] id = PDDLState.getFromStateNFId2ProblemNFId();
+            System.out.println("ID array length: " + id.length);
+            System.out.println(id);
+            if (doctorCostFluent.getId() >= id.length) {
+                throw new RuntimeException("doctorCostFluent ID out of bounds: " + doctorCostFluent.getId() + ", max: " + id.length);
+            }
+            System.out.println("Doctor Cost Fluent ID: " + doctorCostFluent.getId());
+            int costid = id[doctorCostFluent.getId()];
+            // Return the value of the numeric fluent for the "total_doctor_cost" from the state
+            return (double) pddlState.getNumFluents().get(costid);
+        }
+        return 0.0;
+    }
+
+    
 
     @Override
     public Object[] getTransitions(boolean helpful) {
